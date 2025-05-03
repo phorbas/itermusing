57ddbd8933e80b15183768e831f5bcdf91d5964b
Shane Holloway <shane.holloway@ieee.org>
2025-05-03 13:38:49 -0600

    Simplified discovery and separation of concerns between web components

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import rpi_node_resolve from '@rollup/plugin-node-resolve'
 import rpi_jsy from 'rollup-plugin-jsy'
-import rpi_dgnotify from 'rollup-plugin-dgnotify'
 
 const entrypoints = [
   'code/itermusing.jsy',
@@ -13,7 +12,6 @@ const mock_entrypoints = [
 export const _rpis_ = () => [
   rpi_jsy({PLAT_WEB: true}),
   rpi_node_resolve(),
-  rpi_dgnotify(),
 ]
 
 export const external = [
